eef98c0301a2e482bc01c33b0c9b258ab7b9e0d0
imverysorry <ogg3221@gmail.com>
2022-11-19 12:37:06 +0300

    first changes

---
+++ save.php
@@ -0,0 +1,12 @@
+<?php
+
+$connection = require_once './connection.php';
+
+$id = $_POST['id'] ?? '';
+if ($id){
+    $connection->updateNote($id, $_POST);
+} else{
+    $connection->addNote($_POST);
+}
+
+header('Location: index.php');
